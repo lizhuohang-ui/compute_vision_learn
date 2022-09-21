851a82e65a5f701c81e72f0b23ab563d2a6f50ca
FreeBird <2535759837@qq.com>
2022-09-21 22:14:29 +0800

    grad-cam

---
+++ grad-cam/main.py
@@ -0,0 +1,42 @@
+import os
+import numpy as np
+import torch
+from PIL import Image
+import matplotlib.pyplot as plt
+from torchvision import models
+from torchvision import transforms
+from cam_utils import GradCAM, show_cam_on_image
+
+def main():
+    # model = models.resnet34(pretrained=True)
+    model = models.resnet152(pretrained=True)
+    target_layers = [model.layer4]
+
+    data_transform = transforms.Compose([transforms.ToTensor(),
+                                         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+
+    # img_path = "cat.png"
+    img_path = "person.png"
+    assert os.path.exists(img_path), "file: '{}' dose not exist.".format(img_path)
+    img = Image.open(img_path).convert("RGB")
+    img = np.array(img, dtype=np.uint8)
+
+    img_tensor = data_transform(img)
+    input_tensor = torch.unsqueeze(img_tensor, dim=0)
+
+    cam = GradCAM(model=model, target_layers=target_layers, use_cuda=False)
+    target_category = 281
+
+    grayscale_cam = cam(input_tensor=input_tensor, target_category=target_category)
+
+    grayscale_cam = grayscale_cam[0, :]
+    visualization = show_cam_on_image(img.astype(dtype=np.float32) / 255.,
+                                      grayscale_cam,
+                                      use_rgb=True)
+    plt.imshow(visualization)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
+
